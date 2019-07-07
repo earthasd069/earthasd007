a2c4e5988c68deec8d74ffb05b50d50fced4eb5b
earthasd069 <52230717+earthasd069@users.noreply.github.com>
2019-07-07 18:06:24 +0700

    Add files via upload

---
+++ 62060216210161.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	float fahrenheit,celsius;
+	cout <<"Enter fahrenheit ";
+	cin  >>fahrenheit;
+    fahrenheit >= 70 ? cout <<"HOT " :cout <<"Cool\n" ;
+}
