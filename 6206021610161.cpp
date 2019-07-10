0420d5b8ad71b386530cfb9ffba7ed9d9f34d9b3
earthasd069 <52230717+earthasd069@users.noreply.github.com>
2019-07-10 22:13:28 +0700

    Add files via upload

--- 6206021610161.cpp
+++ 6206021610161.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
-int main ()
+
+int main()
+
 {
-	short bath;
-	cout <<"Thaibaths :";
-	cin  >>bath;
-	cout <<endl;
-	cout <<"**Exchange Rate"<< endl;
-	cout <<bath<<" Baths is "<<setprecision(3)<<bath*0.03253<<"dollars.\n";
-	cout <<bath<<" Baths is "<<setprecision(3)<<bath*0.02857<<"Euro\n";
-	return(0);
+	int score,math;
+	cout<< "Enter grade : ";
+	cin >>math;
+	score =math;
+
+	if(score>100) {
+		cout<<("error")<<endl;
+		cin >> score;
+	}
+	
+	else 
+	if((score<=100)&&(score>=90)){
+		cout<<("A")<<endl;
+		cin >>score;}
+	else if((score<=89)&&(score>=80)){
+		cout<<("B\n")<<endl;
+		cin >>score;}
+	
+	else 
+	if((score<=79)&&(score>=70)){
+		cout<<("C\n")<<endl;
+		cin >>score;}
+	
+	else
+	if((score<=69)&&(score>=60)){
+		cout<<("D\n")<<endl;
+		cin >>score;
+	}
+		
+	else
+	if((score>=0)&&(score<=59)){
+		cout<<("F\n")<<endl;
+		cin >>score;
+	}
+
 }
