16119196ef8bc8f419a876d9b1a9b2b4c1b07268
earthasd069 <52230717+earthasd069@users.noreply.github.com>
2019-07-31 15:01:46 +0700

    Add files via upload

--- 6206021610161.cpp
+++ 6206021610161.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
-int main()
-
+int main ()
 {
-	int score,math;
-	cout<< "Enter grade : ";
-	cin >>math;
-	score =math;
+	string Username,Username1;
+	int Password,Password1;
+	char menu;
+	do{
+		cout<<"///////////menu///////////"<<endl;
+		cout<<"1.Register"<<endl;
+		cout<<"2.Login"<<endl;
+		cout<<"Q.Exit Program"<<endl;
+		cout<<"Enter menu"<<endl;
+		cin >> menu;
+		if(menu=='1')
+		{
+			cout<<"*******Register*******"<<endl;
+			cout<<"input Username : " ;
+			cin >> Username;
+			cout<<"input Password : " ;
+			cin >> Password;
 
-	if(score>100) {
-		cout<<("error")<<endl;
-		cin >> score;
-	}
-	
-	else 
-	if((score<=100)&&(score>=90)){
-		cout<<("A")<<endl;
-		cin >>score;}
-	else if((score<=89)&&(score>=80)){
-		cout<<("B\n")<<endl;
-		cin >>score;}
-	
-	else 
-	if((score<=79)&&(score>=70)){
-		cout<<("C\n")<<endl;
-		cin >>score;}
-	
-	else
-	if((score<=69)&&(score>=60)){
-		cout<<("D\n")<<endl;
-		cin >>score;
-	}
-		
-	else
-	if((score>=0)&&(score<=59)){
-		cout<<("F\n")<<endl;
-		cin >>score;
-	}
+		}else if (menu=='2')
+		{
+			cout<<"*******Login*******"<<endl;
+			cout<<"input Username : ";
+			cin >> Username1;
+			cout<<"input Password : ";
+			cin >> Password1;
+			if(Username1!=Username && Password1!=Password)
+			{
+				do
+				{
+					cout<<"!!!!Username or Password incorrect Please try again!!!!"<<endl;
+					cout<<"input Username : ";
+					cin >>Username1;
+					cout<<"input Password :";
+					cin >> Password1;
 
+				}while(Username1!=Username || Password!=Password);
+				cout<<"Username of Password correct ^_^"<<endl;
+
+			}else  cout<<"Username of Password correct ^_^"<<endl;
+		}
+	}while(menu!='Q');
 }
