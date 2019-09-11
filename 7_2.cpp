564375737445cb5db161a912964ad41de8995475
earthasd069 <52230717+earthasd069@users.noreply.github.com>
2019-09-11 20:59:40 +0700

    Update 7_2.cpp

--- 7_2.cpp
+++ 7_2.cpp
@@ -1,16 +1,49 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
-int main()
+void ListStudent(int num[40],string namestu[40],int numsc[40]);
+void Calgrade(int score);
+int i;
+int  main()
 {
-	int A[] = {1,2,3,4,5,6,7,8,9,10};
-	const int Array_Size = 10;
-	int Sum = 0;
-	for(int i = 0 ; i < Array_Size; i++)
-		cout << A[i] << " ";
-	cout<< endl;
-	for(int i = 0; i < Array_Size ; i++)
-		Sum = Sum + A[i];
-	cout << "TOtal of array elements : " << Sum << endl;
-	return 0;
+	int score[40],numstuden[40];
+	string name[40];
+	cout<<"input Number of student : ";
+	cin>>numstuden[0];
+	for(i = 0;i < numstuden[0];i++)
+	{
+		cout<<"input Name : ";
+		cin>>name[i];
+		cout<<"input score "<< i+1 <<" : ";
+		cin>>score[i];
+		cout<<endl;
+	}
+	ListStudent(numstuden,name,score);
+	return (0);
 }
-	
+void ListStudent(int nums[40],string namestu[40],int numsc[40])
+{
+	for(i = 0;i < nums[0];i++)
+	{
+		cout<<"studen Name : "<< namestu[i] <<" Score "<<i+1 <<" = "<<numsc[i];
+		Calgrade(numsc[i]);
+	}
+}
+void Calgrade(int score)
+{
+	if(score >= 50 && score <= 64)
+	{
+		cout<<" You grade is D "<<endl;
+	}
+	else if(score >= 65 && score <= 79)
+	{
+		cout<<" You grade is C "<<endl;
+	}
+	else if(score >= 80 && score <=89)
+	{
+		cout<<" You grade is B "<<endl;
+	}
+	else
+	{
+		cout<<" You grade is A "<<endl;
+	}
